6590368fb7c7c2bf359d5d956b5ca002276a2ed8
AleksiKantola3 <aleksi3472@gmail.com>
2024-12-15 10:30:17 +0200

    New beginning for project

---
+++ fbmech.py
@@ -0,0 +1,9 @@
+
+# This is the container class for keeping the statistics for individual battlemechs
+class FbMech:
+    def __init__(self, mechname, bv, elitebv, rookiebv):
+        self.mechname = mechname
+        self.bv = bv
+        self.elitebv = elitebv
+        self.rookiebv = rookiebv
+
---
+++ forcebuilder_main.py
@@ -0,0 +1,6 @@
+import fbmech
+
+#this is a tool for giving a player of Classic Battletech Tabletop war game good a good idea for their force building options
+#One inputs their battlemech roster and available pilots and as an output gets valid lists for their force
+
+
